6f04c3ddd167a86a4ff3e2bb8bf651508a725d79
Antony Bragg <antonybragg@gmail.com>
2022-01-21 20:15:27 +0000

    Update Write-Log.psm1
    Fixed helper statements
    Added log validation
    Added option date format parameter

--- Module/Write-Log/Write-Log.psm1
+++ Module/Write-Log/Write-Log.psm1
@@ -1,24 +1,24 @@
 <#
 .SYNOPSIS
-Creates entries to a log file
+Creates entries to a log file.
 
 .DESCRIPTION
 Creates an entry of a specified severity into the log file.
 
 .OUTPUTS
-A *.log file with specific name or location (Default is script root named log.log)
+A *.log or *.txt file with specific name or location.
 
 .PARAMETER Message
 The log message entered into the log file.
 
 .PARAMETER severity
-The severity set for the log message within the log file. (Info, Warning, Error)
+The severity set for the log message within the log file. (Info, Warning, Error).
 
 .PARAMETER logLocation
 The directory of the log file.
 
-.PARAMETER logName
-The name of the log file. Should be a *.log extension. The default location is the area the script was executed.
+.PARAMETER DateFormat
+String format if a specific date format is desired.
 
 .EXAMPLE
 write-log -Message "Oh no Jurgen was here"
@@ -40,15 +40,29 @@ Function Write-Log {
       [ValidateSet("Info", "Warning", "Error")]
       [string]$severity = "Info",
 
-      [Parameter(Position=2, HelpMessage="Directory for the log file.")]
-      [string]$logLocation = "$PSScriptRoot\log.log"
+      [Parameter(Position=2, HelpMessage="Directory for the log file. Extension must be .log or .txt")]
+      [string]$logLocation = "$PSScriptRoot\Events.log",
+
+      [Parameter(Position=3, HelpMessage="String format of the date. E.g. dd/MM/yyyy")]
+      [string]$DateFormat
   )
+
+  $extension = [System.IO.Path]::GetExtension($logLocation)
+  if(!($extension -eq ".log") -and !($extension -eq ".txt")) {
+    throw "Invalid log file extension. Please use .log or .txt" 
+  }
 
   if(!(test-path $logLocation)) {
     new-item $logLocation -Force | out-null
   }
-  
-  $timeStamp = Get-date -Format "dd/MM/yyyy HH:mm:ss"
+
+  if($null -eq $DateFormat) {
+    [DateTime]$Date = Get-Date
+    $timeStamp = $date.ToShortDateString() + " " + $date.ToShortTimeString()
+  } else {
+      $timeStamp = Get-date -Format $DateFormat
+  }
+
   $Output = "$timeStamp - [$severity] $Message"
   Add-Content $logLocation -value $Output
   Write-Host "$Output"
